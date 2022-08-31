3d15f9c5ec1e311b7c7bbd31dd2bd64c72c4aab7
Valentin Duffet <vavadu07@gmail.com>
2022-08-31 22:21:23 +0200

    prettier

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,28 +1,28 @@
 let displayLightbox = (media) => {
-    console.log(media);
+  console.log(media);
 
-    // Open modal
-    const lightbox = document.getElementById("lightbox");
-	lightbox.style.display = "block";
+  // Open modal
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'block';
 
-    // Close modal when press echap
-    window.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape') {
-            closeLightbox();
-        }
-      })
+  // Close modal when press echap
+  window.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      closeLightbox();
+    }
+  });
 
-    const containerImg = document.querySelector(".panel-center-lightbox");
-    const img = document.createElement( 'img' );
-    img.setAttribute("src", `assets/images/${media.image}`);
-    img.setAttribute("id", `media`);
-    containerImg.appendChild(img);
-}
+  const containerImg = document.querySelector('.panel-center-lightbox');
+  const img = document.createElement('img');
+  img.setAttribute('src', `assets/images/${media.image}`);
+  img.setAttribute('id', `media`);
+  containerImg.appendChild(img);
+};
 
 let closeLightbox = () => {
-    const lightbox = document.getElementById("lightbox");
-	lightbox.style.display = "none";
+  const lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'none';
 
-    const containerImg = document.querySelector(".panel-center-lightbox");
-    containerImg.removeChild(containerImg.lastChild);
-}
+  const containerImg = document.querySelector('.panel-center-lightbox');
+  containerImg.removeChild(containerImg.lastChild);
+};
